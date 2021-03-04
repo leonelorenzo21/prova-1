7f2a936bb75fb56b512595f4559bec592b91931b
leone.lorenzo <leone.lorenzo@TAVE-PC18.OSDBSESTO.local>
2021-03-04 12:26:41 +0100

    add welcomm message

--- prova 1/src/prova/pkg1/Prova1.java
+++ prova 1/src/prova/pkg1/Prova1.java
@@ -16,6 +16,8 @@ public class Prova1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("bomboclat");
+        
     }
     
 }
